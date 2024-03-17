1b9bd286eedb8dd0dc039b8315d90d15630bf2aa
alessandro <alessandrodamata6@gmail.com>
2024-03-17 17:56:17 -0300

    feat: estylizando component Header e page home

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,7 +2,7 @@
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
-import {MenuIcon} from "lucide-react";
+import {HomeIcon, MenuIcon} from "lucide-react";
 import Logo from "@/assets/logo.png";
 import { Link } from "react-router-dom";
 
@@ -16,14 +16,94 @@ const Header = () => {
 				</Link>
 			</div>
 			<Sheet>
-				<SheetTrigger asChild >
+				<SheetTrigger asChild  >
 					<Button className="w-10 h-10" size={"icon"} variant={"ghost"}>
 						<MenuIcon className="text-primary"/>
 					</Button>
 				</SheetTrigger>
-				
-				<SheetContent className="w-72 bg-secondary" >
-					
+		
+				<SheetContent className="w-72 bg-primary border-l-0" >
+					<SheetHeader className="items-center mt-4">
+						<text className="text-white">Menu</text>
+					</SheetHeader>
+					<div className="flex flex-col gap-4 py-4 ">
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar AES
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Decriptografar AES
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar MD5
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar SHA1
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar SHA256
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar 512
+								</Button>
+							</Link>
+						</SheetClose>
+
+						<SheetClose asChild>
+							<Link to={"/"}>
+								<Button
+									variant={"outline"}
+									className="w-full items-center gap-2"
+								>
+								Criptografar ripemd160
+								</Button>
+							</Link>
+						</SheetClose>
+					</div>
 				</SheetContent>
 			</Sheet>
 		</Card>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
 
 const Home = () => {
 	return (
 		<div >
 			<text className="font-bold text-base text-red-600">Home</text>
-			<Button>Click me</Button>
+
 		</div>
 	);
 };
